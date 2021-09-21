d272df3cd07c05dc689edb70182daf4f5278de43
onava <enava23@yahoo.com>
2021-09-20 23:23:11 -0400

    react-router-dom installed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+
+    </Router>
   );
 }
 
